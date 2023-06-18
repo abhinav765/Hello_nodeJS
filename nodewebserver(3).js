950c54b5520fecd2516dd5cddd759f290199481f
AbhinavDubey <abhinavdubey7024@gmail.com>
2023-06-18 19:11:22 +0530

    first commit

---
+++ nodewebserver(3).js
@@ -0,0 +1,13 @@
+var h = require('http')
+
+var server = h.createServer(function(req,res){
+	
+		res.writeHead(200,{'content-type':'text/html'})
+		res.write("<h1>Hello from Node js</h1>")
+		res.end();
+		})
+
+server.listen(9000,function(){
+	console.log("started at port number 9000")
+
+})
